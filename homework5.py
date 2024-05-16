09e846a1e621f259f55183f60b0b148aba8bbab9
Vivid156 <167762620+Vivid156@users.noreply.github.com>
2024-05-16 19:36:08 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,11 @@
+my_list = ['apple', 'pineapple', 'orange', 'melon', 'pear', 'watermelon']
+print(my_list)
+print(my_list [0], my_list[-1])
+print(my_list [2:5])
+my_list[2] = 'berry'
+print(my_list)
+my_dict = {'string' : 'строка', 'water' : 'вода', 'salt' : 'соль'}
+print(my_dict)
+print(my_dict.get('water'))
+my_dict['water'] = 'moter'
+print(my_dict)
